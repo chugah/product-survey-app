e47c1c3b49092f26222fa26665f9b6009a83c4bb
Sami Barghshoon <sdbar@Samis-MacBook-Pro.local>
2018-12-27 17:19:16 -0500

    verify a user when signing in

--- services/passport.js
+++ services/passport.js
@@ -3,8 +3,22 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+
+const localOptions = { usernameField: 'email'};
+const localLogin = new LocalStrategy(localOptions, function (email, password, done) {
+  User.findOne({ email: email }, function(err, user) {
+    if(err) { return done(err, false); }
+    if(!user) { return done(null, false); }
+    user.comparePassword(password, function(err, isMatch) {
+      if(err) { return done(err); }
+      if(!isMatch) { return done(null, false); }
+      return done(null, user);
+    });
+  });
+});
 
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromHeader('authorization'),
@@ -23,6 +37,7 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 });
 
 passport.use(jwtLogin);
+passport.use(localLogin);
 
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
