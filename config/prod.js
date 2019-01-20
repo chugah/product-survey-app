9d0ad485c6889e95a79e52323951f9c6be31d064
Sami Barghshoon <sdbar@Samis-MacBook-Pro.local>
2019-01-20 13:17:59 -0500

    added env process variables

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  linkedinClientID: process.env.LINKEDIN_CLIENT_ID,
+  linkedinClientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+  cookieKey: process.env.COOKIE_KEY,
+  mongoURI: process.env.MONGO_URI
+};
